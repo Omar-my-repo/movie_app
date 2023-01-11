008c2e0f2f1a4d2671807347ec1605b5ecc8e173
Dsc-tasks <clinicawy@gmail.com>
2023-01-12 00:14:47 +0200

    + comments

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    //edited by omar
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: HomeLayout.routeName,
